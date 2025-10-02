59d09e97203d0209000a225a3b13198cb0647f09
chubyx <carlosduquebravo@gmail.com>
2025-10-02 19:54:14 -0300

    configuraciones finales

--- app/src/main/java/com/devst/app/AcercaDeActivity.java
+++ app/src/main/java/com/devst/app/AcercaDeActivity.java
@@ -1,6 +1,10 @@
-package com.example.miapp;
+package com.devst.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AcercaDeActivity extends AppCompatActivity {
@@ -9,5 +13,14 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        Button btnIrGithub = findViewById(R.id.btnIrGithub);
+
+        btnIrGithub.setOnClickListener(v -> {
+            // Aquí ya usamos el implícito, pero DENTRO de la activity explícita
+            String url = "https://github.com/chubyx";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/devst/app/PerfilActivity.java
+++ app/src/main/java/com/devst/app/PerfilActivity.java
@@ -22,5 +22,6 @@ public class PerfilActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+
     }
 }
